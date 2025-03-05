b10d7913927451fe3613335804aba23d9307ae94
marroqqq <alejandromarroqui2004@gmail.com>
2025-03-05 13:06:42 +0100

    Update ejercicio4.js

---
+++ ejercicio4.js
@@ -0,0 +1,9 @@
+//Dado el siguiente array:
+const aldeanos = ['Fibrilio', 'Narciso', 'Vacarena', 'Tendo', 'Nendo']
+//4.1 - Saca a "Tendo" por consola atacando su posición.
+//4.2 - Coloca en el último lugar de este array a "Cervasio".
+//4.3 - Cambia el primer elemento de este array por "Bambina".
+//4.4 - Dale la vuelta a este array.
+//4.5 - Cambia a "Narciso" por "Canela" haciendo uso de un método de array.
+//4.6 - Imprime por consola el último elemento de este array sin atacar a la posición explicitamente
+// Pista: puedes usar el método length con algo más para ello.
